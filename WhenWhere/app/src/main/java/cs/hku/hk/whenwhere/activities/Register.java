7a06b25f0a37b622976d3ec8c27dd2f7bcb915cc
7D24H <31737338+7D24H@users.noreply.github.com>
2019-03-27 22:21:14 +0800

    register.xml finish

---
+++ WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/activities/Register.java
@@ -0,0 +1,15 @@
+package cs.hku.hk.whenwhere.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cs.hku.hk.whenwhere.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
