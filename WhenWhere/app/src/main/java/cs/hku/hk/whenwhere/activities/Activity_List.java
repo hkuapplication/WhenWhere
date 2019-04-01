ec76b502895f464cc560a2afaf9882026a2d1028
yitongYao <yao19981229@163.com>
2019-04-01 19:06:47 +0800

    差sql取数据
    但是还是不太懂list怎么放进去的，或许和recycleview的机制有关

---
+++ WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/activities/Activity_List.java
@@ -0,0 +1,57 @@
+package cs.hku.hk.whenwhere.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cs.hku.hk.whenwhere.R;
+import cs.hku.hk.whenwhere.adapters.Activity_recycle;
+import cs.hku.hk.whenwhere.model.Activities;
+
+public class Activity_List extends AppCompatActivity {
+    private AppCompatActivity activity=Activity_List.this;
+    private RecyclerView recyclerView;
+    private List<Activities> listActivities;
+    private Activity_recycle activity_recycle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        getSupportActionBar().setTitle("");
+        initViews();
+        initObjects();
+
+    }
+
+    private void initObjects() {
+        listActivities = new ArrayList<>();
+        activity_recycle = new Activity_recycle(listActivities);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(activity_recycle);
+        //databaseHelper = new DatabaseHelper(activity);
+
+        //String emailFromIntent = getIntent().getStringExtra("EMAIL");
+       // textViewName.setText(emailFromIntent);
+
+        getDataFromMYSQL();
+    }
+
+    private void getDataFromMYSQL() {
+
+    }
+
+    private void initViews() {
+        recyclerView = findViewById(R.id.recyclerViewUsers);
+    }
+}
---
+++ WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/model/Activities.java
@@ -0,0 +1,40 @@
+package cs.hku.hk.whenwhere.model;
+
+public class Activities {
+    private int id;
+    private String name;
+    private String place;
+    private String time;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
