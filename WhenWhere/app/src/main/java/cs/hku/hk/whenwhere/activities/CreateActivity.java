28bcc24580f5546e6426bfc7e00cd0ef67c56344
Scarlett XU <38967428+scarlettwwx@users.noreply.github.com>
2019-04-15 10:29:48 +0800

    Create CreateActivity.java

---
+++ WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/activities/CreateActivity.java
@@ -0,0 +1,13 @@
+package cs.hku.hk.whenwhere.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import cs.hku.hk.whenwhere.R;
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_activity);
+    }
+}
