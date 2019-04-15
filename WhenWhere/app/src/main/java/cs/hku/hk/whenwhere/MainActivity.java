a8e3f27f83f97368b28bee6dba60db5ede238d37
Scarlett XU <38967428+scarlettwwx@users.noreply.github.com>
2019-04-15 11:38:32 +0800

    Update MainActivity.java

--- WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/MainActivity.java
+++ WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/MainActivity.java
@@ -1,13 +1,23 @@
 package cs.hku.hk.whenwhere;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import cs.hku.hk.whenwhere.activities.CalendarFragment;
+import cs.hku.hk.whenwhere.activities.CreateActivity;
+import cs.hku.hk.whenwhere.activities.ListActivity;
+import cs.hku.hk.whenwhere.activities.Register;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 }
