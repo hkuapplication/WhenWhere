7e9633b63e2c80f072cf4af86cb792d4f672ad51
Scarlett XU <38967428+scarlettwwx@users.noreply.github.com>
2019-04-15 10:32:49 +0800

    Update MemberFragment.java

--- WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/activities/MemberFragment.java
+++ WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/activities/MemberFragment.java
@@ -1,5 +1,6 @@
 package cs.hku.hk.whenwhere.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +8,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import cs.hku.hk.whenwhere.R;
 
 public class MemberFragment extends Fragment {
 
+
+    Button addButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.inner_fragment_member,null);
+        View view = inflater.inflate(R.layout.member_list,null);
+
+        addButton = (Button)view.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (getActivity(),AddMemberActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
