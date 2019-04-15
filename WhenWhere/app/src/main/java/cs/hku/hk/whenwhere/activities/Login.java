a6db50d340fe5fa5b3148f53537d583fb8eeef20
Scarlett XU <38967428+scarlettwwx@users.noreply.github.com>
2019-04-15 11:22:38 +0800

    need to check valid login request

--- WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/activities/Login.java
+++ WhenWhere/app/src/main/java/cs/hku/hk/whenwhere/activities/Login.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import cs.hku.hk.whenwhere.R;
 
 
@@ -17,23 +19,35 @@ public class Login extends AppCompatActivity {
     AppCompatButton button1;
     TextInputEditText email;
     TextInputEditText password;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         button1 = (AppCompatButton)findViewById(R.id.loginButton);
         email=(TextInputEditText)findViewById(R.id.textInputEditTextEmail);
+        tv=(TextView)findViewById(R.id.textViewLinkRegister) ;
         password=(TextInputEditText)findViewById(R.id.textInputEditTextPassword);
         String mEmail = email.getText().toString();
         String myPassword = password.getText().toString();
-        
-        //determine whether login is successful.....
-        //if succeed jump to homepage immediately
+
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //determine whether login is successful.....
+                //if succeed jump to homepage immediately else Toast maketext "Sorry, your username or password may be incorrect"
                 Intent intent=new Intent();
                 intent.setClass(Login.this, CalendarFragment.class);
+                startActivity(intent);
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(Login.this, Register.class);
                 startActivity(intent);
             }
         });
@@ -41,3 +55,4 @@ public class Login extends AppCompatActivity {
 
 
 }
+
